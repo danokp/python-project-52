0d8bb1f45b040fb54d8ed961ab1cf625b29c8fae
Daniil <danokp@mail.ru>
2023-05-24 16:03:08 +0400

    Add i18n - translation to Russian

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,6 +14,8 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from django.contrib.messages import constants as messages
+from django.utils.translation import gettext_lazy as _
 
 
 load_dotenv()
@@ -46,12 +48,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'task_manager'
 ]
+
+MESSAGE_TAGS = {
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.ERROR: 'alert-danger',
+ }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -113,13 +123,22 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-Ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGES = [
+    ('ru', _('Russian')),
+    ('en', _('English')),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale/',
+]
 
 
 # Static files (CSS, JavaScript, Images)
