afa682291ee0e6c4602b93d79bc936d8ba7ab30b
Daniil <danokp@mail.ru>
2023-06-20 22:13:06 +0400

    Update users/form.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,12 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
+# from django.utils.translation import gettext as _
+# from django import forms
 
 from .models import User
 
 
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(label='First name')
-    last_name = forms.CharField(label='Last name')
+    # first_name = forms.CharField(label=_('First name'))
+    # last_name = forms.CharField(label='Last name')
     class Meta():
         model = User
-        fields = ['first_name', 'last_name', 'username']
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'password1',
+            'password2',
+        ]
