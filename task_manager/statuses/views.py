f2f985439b19763d08057951c9b7843171a04241
Daniil <danokp@mail.ru>
2023-06-22 17:24:37 +0400

    Add urls for status application

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class StatusView(View):
+    '''Show list of statuses.'''
+
+    pass
+
+
+class StatusCreateView(View):
+    '''Show list of statuses.'''
+
+    pass
+
+
+class StatusUpdateView(View):
+    '''Show list of statuses.'''
+
+    pass
+
+
+class StatusDeleteView(View):
+    '''Show list of statuses.'''
+
+    pass
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -23,6 +23,7 @@ from task_manager import views
 urlpatterns = i18n_patterns(
     path('', views.HomePageView.as_view(), name='homepage'),
     path('users/', include('task_manager.users.urls')),
+    path('statuses/', include('task_manager.statuses.urls')),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
