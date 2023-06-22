980b247c05aa9f6ab2b6a6606c01aefa4e9c9c61
Daniil <danokp@mail.ru>
2023-06-22 18:42:45 +0400

    Add created_at to Status model

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Status(models.Model):
+    '''Status model'''
     name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __srt__(self):
         return self.name
