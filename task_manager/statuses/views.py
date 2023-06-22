00d28a2594edf9ce24a089146cd1440f6910db9f
Daniil <danokp@mail.ru>
2023-06-22 20:47:33 +0400

    Add status update page

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -46,13 +46,35 @@ class StatusCreateView(View):
         )
 
 
-
-
-
 class StatusUpdateView(View):
     '''Update status.'''
 
-    pass
+    def get(self, request, *args, **kwargs):
+        status_id = kwargs.get('pk')
+        status = Status.objects.get(id=status_id)
+        form = StatusCreationForm(instance=status)
+        button_text = _('Update')
+        return render(
+            request,
+            'statuses/update_status.html',
+            context={'form': form, 'button_text': button_text},
+        )
+
+    def post(self, request, *args, **kwargs):
+        status_id = kwargs.get('pk')
+        status = Status.objects.get(id=status_id)
+        form = StatusCreationForm(request.POST, instance=status)
+
+        if form.is_valid():
+            messages.success(request, _('Статус успешно изменен'))
+            form.save()
+            return redirect('show_statuses')
+        button_text = _('Update')
+        return render(
+            request,
+            'statuses/update_status.html',
+            context={'form': form, 'button_text': button_text},
+        )
 
 
 class StatusDeleteView(View):
