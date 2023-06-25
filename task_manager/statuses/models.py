360797fcb4e28ad06ca2d7c4fb2ef2b68ce2cfbf
Daniil <danokp@mail.ru>
2023-06-25 20:45:48 +0400

    Fix mistake in __str__ method name

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -6,5 +6,5 @@ class Status(models.Model):
     name = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __srt__(self):
+    def __str__(self):
         return self.name
