73991bc2b17f3fd8a4adbd3e7ac3b538b73a5867
Daniil <danokp@mail.ru>
2023-06-30 13:46:55 +0400

    Change button_text to "Войти"

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,9 +1,7 @@
 from django.views.generic.base import TemplateView
 from django.contrib import messages
 from django.utils.translation import gettext as _
-from django.utils.translation import pgettext
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import reverse_lazy
 
 
 class HomePageView(TemplateView):
@@ -16,7 +14,7 @@ class UserLoginView(LoginView):
     '''Authorise the user.'''
 
     template_name = 'login.html'
-    extra_context = {'button_text': pgettext('Button name', 'Login')}
+    extra_context = {'button_text': 'Войти'}
 
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
