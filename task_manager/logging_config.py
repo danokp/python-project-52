e09a3ebc061bf1821a83c0a74b35316b5bbe7cad
Daniil <danokp@mail.ru>
2023-07-01 17:50:13 +0400

    Change looger format

--- task_manager/logging_config.py
+++ task_manager/logging_config.py
@@ -9,9 +9,7 @@ c_handler = logging.StreamHandler()
 c_handler.setLevel(logging.WARNING)
 
 # Create formatter and add it to handler
-c_format = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
+c_format = logging.Formatter('%(levelname)s | %(message)s')
 c_handler.setFormatter(c_format)
 
 # Add handlers to the logger
