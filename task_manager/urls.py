7e63fea83dc71613de442d8dbf7eaa412cffcb12
Daniil <danokp@mail.ru>
2023-07-07 07:54:48 +0400

    Delete i18n_patterns from task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.i18n import i18n_patterns
+# from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from task_manager import views
 
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path('', views.HomePageView.as_view(), name='homepage'),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
@@ -29,4 +29,4 @@ urlpatterns = i18n_patterns(
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-)
+]
